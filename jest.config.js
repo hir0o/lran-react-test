0c8915b34e9bc96672ae1795056110a307dd4046
hir0o <mk.9.q.kyu@gmail.com>
2023-01-18 17:08:15 +0900

    カバレッジ見るよ

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  globals: {
-    "ts-jest": {
-      tsconfig: "./tsconfig.test.json",
-    },
-  },
-  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+	preset: "ts-jest",
+	testEnvironment: "jsdom",
+	globals: {
+		"ts-jest": {
+			tsconfig: "./tsconfig.test.json",
+		},
+	},
+	setupFilesAfterEnv: ["./src/jest.setup.ts"],
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
 };
